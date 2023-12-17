a2c763becc22e0b0d18c3279ecfdabcb9155f43c
albkli <albertklimowicz@gmail.com>
2023-12-17 14:25:20 +0100

    aktu

--- 1_dziedziczenie.py
+++ 1_dziedziczenie.py
@@ -1,6 +1,22 @@
 class Animal:
-    def __init__(self, rodzaj, kiedy_pracyhe):
+    def __init__(self, rodzaj, kiedy_pracuje, wiek):
         self.gatunek = rodzaj
+        self.tryb = kiedy_pracuje
+        self.data_urodzenia = 2023 - wiek
 
+    def krzycz(self):
+        print("Gatunek to " + self.gatunek, )
 
-zwierze01 = Animal ('papugA', 'day')
+zwierze01 = Animal ('papuga', 'day', 12)
+
+zwierze01.krzycz()
+
+class Bird(Animal):
+    def __init__(self, tryb):
+        super().__init__('ptak', tryb, 10)
+
+ptak01 = Bird('dzienny')
+
+print(ptak01.data_urodzenia)
+
+ptak01.krzycz()
